097ad887b17bbe2de1fdd108b7c0b6963b431d96
Cris Henderson <cris@infugia.com>
2023-02-15 17:19:18 -0600

    space out js consts

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 const { Configuration, OpenAIApi } = require("openai");
+
 const configuration = new Conifguration({
     apikey: process.env.OPENAI_API_KEY,
 });
+
 const apenai = new OpenAIApi(configuration);
+
 const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: "Say this is a test",
