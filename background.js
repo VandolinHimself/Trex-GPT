c0d0f951e0d554073eae05b931c76f4be6a9a528
Cris Henderson <cris@infugia.com>
2023-02-15 18:00:29 -0600

    more background.js cleanup

--- background.js
+++ background.js
@@ -11,15 +11,17 @@ const uid = () => {
 
 	const generateXes = (count) => {
 		let result = '';
+
 		for(let i = 0; i < count; ++i) {
 			result += generateX();
 		}
+
 		return result;
 	}
 
 	const generateconstant = () => {
 		const value = generateNumber(16);
-		const constant =  (value & 0x3) | 0x8;
+		const constant = (value & 0x3) | 0x8;
 		return constant.toString(16);
 	}
     
@@ -45,7 +47,7 @@ const getToken = async () => {
         }
 
         try {
-            const data = await resp.json()
+            const data = await resp.json();
 
             if (!data.accessToken) {
                 reject('ERROR');
@@ -68,7 +70,7 @@ const getResponse = async (question) => {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": "Bearer " + accessToken,
+                    "Authorization": "Bearer " + accessToken
                 },
                 body: JSON.stringify({
                     action: "next",
@@ -86,7 +88,8 @@ const getResponse = async (question) => {
                     parent_message_id: uid()
                 });
             });
-            resolve(res.body)
+
+            resolve(res.body);
         }
         catch (e) {
             if (e === "CLOUDFLARE") {
@@ -103,7 +106,7 @@ chrome.runtime.onConnect.addListener((port) => {
     port.onMessage.addListener((msg) => {
         const question = msg.question;
 
-        getResponse(question).then(async answer => {
+        getResponse(question).then(async (answer) => {
             const resRead = answer.getReader();
 
             while (true) {
